9c74a26ebecc034731e4d6c1419bda745072cc1a
eduaardo912 <eduaardo912@gmail.com>
2024-03-11 10:15:31 -0500

    Agregado manejador de excepciones y ajuste en config seguridad

--- src/main/java/com/bluesoft/servicebank/util/SecurityUtil.java
+++ src/main/java/com/bluesoft/servicebank/util/SecurityUtil.java
@@ -1,5 +1,6 @@
 package com.bluesoft.servicebank.util;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,11 @@ public class SecurityUtil {
     public static final String AUTH_HEADER = "authorization";
     public static final String AUTH_TOKEN_TYPE = "Bearer";
     public static final String AUTH_TOKEN_PREFIX = AUTH_TOKEN_TYPE + " ";
+
+    public static SimpleGrantedAuthority convertToAuthority(String role) {
+        String formattedRole = role.startsWith(ROLE_PREFIX) ? role : ROLE_PREFIX + role;
+        return new SimpleGrantedAuthority(formattedRole);
+    }
 
     public static String getAuthTokenFromRequest(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTH_HEADER);
